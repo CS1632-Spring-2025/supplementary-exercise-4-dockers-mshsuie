95a6fe1633a7192ced77d4a7134f158b416af828
mshsuie <mshsuie@gmail.com>
2025-04-08 16:58:29 -0400

    Update Selenium tests to run in headless mode

--- selenium/src/test/java/edu/pitt/cs/D3Test.java
+++ selenium/src/test/java/edu/pitt/cs/D3Test.java
@@ -37,9 +37,9 @@ public class D3Test {
 
   @Before
   public void setUp() {
-    driver = new ChromeDriver();
-    js = (JavascriptExecutor) driver;
-    vars = new HashMap<String, Object>();
+    ChromeOptions options = new ChromeOptions();
+    options.addArguments("--headless");
+    driver = new ChromeDriver(options);
   }
 
   @After
